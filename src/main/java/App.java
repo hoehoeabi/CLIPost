09a0fbad7d1725fc1ce3a87e7121c8d40bd8ba06
hoehoeabi <0203jyh00@naver.com>
2026-04-17 01:46:16 +0900

    refactor : 명령어 받는 부분을 switch -> map으로 변경
    - 명령어가 많아지니 switch문은 가독성이 떨어진다고 판단
    - 하여 initCommandMap으로 미리 명령어와 해당 실행 구문을 map에 담고 꺼내오는 방식으로 변경

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,34 @@
 import controller.ArticleController;
 import global.dto.Rq;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class App {
 
     private final Scanner sc;
+    private final Map<String, Consumer<Rq>> commandMap = new HashMap<>();
 
     public App(Scanner sc) { // 외부에서 주입받도록 변경
         this.sc = sc;
     }
 
+    private void initCommandMap(ArticleController articleController) {
+        commandMap.put("write", rq -> articleController.doWrite()); // doWrite()는 인자가 없으므로 람다 처리
+        commandMap.put("list", articleController::showList);
+        commandMap.put("detail", articleController::showDetail);
+        commandMap.put("update", articleController::doModify);
+        commandMap.put("delete", articleController::doDelete);
+        commandMap.put("search", articleController::doSearch);
+        commandMap.put("help", rq -> articleController.showHelp()); // doWrite()랑 동일
+    }
+
     public void run() {
         Container.init(this.sc);
         ArticleController articleController = Container.getArticleController();
+        initCommandMap(articleController);
 
         System.out.println("== 자바 텍스트 게시판 시작 ==");
         articleController.showHelp();
@@ -24,22 +39,21 @@ public class App {
             if (command.isEmpty()) continue;
 
             Rq rq = new Rq(command);
+            String actionPath = rq.getActionPath();
 
-            switch (rq.getActionPath()) {
-                case "exit" -> {
-                    System.out.println("프로그램을 종료합니다.");
-                    Container.scClose();
-                    return;
-                }
-                case "write" -> articleController.doWrite();
-                case "list" -> articleController.showList(rq);
-                case "detail" -> articleController.showDetail(rq);
-                case "update" -> articleController.doModify(rq);
-                case "delete" -> articleController.doDelete(rq);
-                case "search" -> articleController.doSearch(rq);
-                case "help" -> articleController.showHelp();
-                default -> System.out.println("존재하지 않는 명령어입니다. 'help'를 입력해보세요.");
+            if (actionPath.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                Container.scClose();
+                break;
             }
+
+            Consumer<Rq> action = commandMap.get(actionPath);
+            if (action != null) {
+                action.accept(rq);
+            } else {
+                System.out.println("존재하지 않는 명령어입니다. 'help'를 입력해보세요.");
+            }
+
         }
     }
 }
